2ffa8d7fd703f92378b502f952391ba2eca7a8fb
Anas <anasmahmud523@gmail.com>
2023-01-11 00:35:08 +0600

    sidenav

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -4,60 +4,57 @@ import { FiHome } from "react-icons/fi";
 import { FaRegUser } from "react-icons/fa";
 import { BiMessageSquareDetail } from "react-icons/bi";
 import { IoBookmarksOutline } from "react-icons/io5";
-import { FaPenNib } from "react-icons/fa";
+import { FaPenNib, FaGithub } from "react-icons/fa";
+import { TbDownload } from "react-icons/tb";
+import { TfiLinkedin } from "react-icons/tfi";
 
 const Navbar = () => {
     return (
         <div className='bg-slate-800'>
-            <header aria-label="Site Header" >
-                <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
-                    <div className="flex flex-col items-center ">
-                        <div className="md:flex md:items-center md:gap-12">
-                            <h1 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text lg:text-3xl font-extrabold text-transparent sm:text-5xl">
-                                iamanas
-                            </h1>
-                        </div>
+            <div className="sticky top-0 h-fit hidden lg:block px-4 sm:px-6 lg:px-8">
+                <div className="nav flex flex-col h-[100vh] items-center justify-around">
 
-                        <div className="hidden md:block">
-                            <nav aria-label="Site Nav">
-                                <ul className="flex flex-col gap-10">
-                                    <li>
-                                        <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
-                                            <FiHome />
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
-                                            <IoBookmarksOutline />
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
-                                            <FaRegUser />
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
-                                            <BiMessageSquareDetail />
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
-                                            <FaPenNib />
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
+                    <h1 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text leading-none lg:text-3xl font-extrabold text-transparent sm:text-5xl">
+                        iam<br/>anas
+                    </h1>
 
-                        <div className="flex items-center gap-4">
-                            <div className="sm:flex sm:gap-4">
-                                <a href='https://drive.google.com/file/d/1i738VtBjLDH4JISkbcQLe0uVbcWYTjsE/view?usp=share_link' target='_blank' rel="noreferrer" className="rounded-md bg-gradient-to-r from-blue-500 to-purple-600 px-5 py-2.5 text-sm font-medium text-white shadow">
-                                    Resume
-                                </a>
-                            </div>
+                    <div className="">
+                        <ul className="flex flex-col gap-8 text-3xl">
+                            <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
+                                <FiHome className='' />
+                            </Link>
+                            <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
+                                <IoBookmarksOutline />
+                            </Link>
+                            <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
+                                <FaRegUser />
+                            </Link>
+                            <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
+                                <BiMessageSquareDetail />
+                            </Link>
+                            <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
+                                <FaPenNib />
+                            </Link>
+                        </ul>
+                    </div>
 
-                            {/* <div className="block md:hidden">
+                    <div className="">
+                        <ul className="flex flex-col gap-8 text-3xl">
+                            <a className="text-gray-300 font-semibold transition hover:text-green-300/75" href='https://drive.google.com/file/d/1i738VtBjLDH4JISkbcQLe0uVbcWYTjsE/view?usp=share_link' target='_blank' rel="noopener noreferrer">
+                                <TbDownload />
+                            </a>
+                            <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
+                                <FaGithub />
+                            </Link>
+                            <Link className="text-gray-300 font-semibold transition hover:text-green-300/75">
+                                <TfiLinkedin />
+                            </Link>
+                            <Link className="text-gray-300 text-2xl font-semibold transition hover:text-green-300/75">
+                                <FaPenNib />
+                            </Link>
+                        </ul>
+
+                        {/* <div className="block md:hidden">
                                 <button
                                     className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
                                 >
@@ -77,10 +74,9 @@ const Navbar = () => {
                                     </svg>
                                 </button>
                             </div> */}
-                        </div>
                     </div>
                 </div>
-            </header>
+            </div>
 
         </div>
     );
