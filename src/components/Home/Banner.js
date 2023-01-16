0df574fb5bea74025546fe0492c5f8cbb6988d0d
Anas <anasmahmud523@gmail.com>
2023-01-16 17:32:31 +0600

    banner update

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -2,28 +2,25 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div id='home' className=''>
-            <section class="bg-gray-900 text-white">
+        <section id='home'>
+            <div class="w-full mx-auto pl:0  pt-16 h-[100vh] lg:h-screen">
 
-                <div class="w-full mx-auto pl:0  pt-16 h-[100vh] lg:h-screen">
-
-                    <div class="lg:h-[50vh] h-[40vh] grid place-content-center">
-
-                        <span>Hello i am</span>
-                        <h1 class="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text lg:text-5xl font-extrabold text-transparent">
-                            Anas Mahmud.
+                <div class="lg:h-[60vh] h-[40vh] flex items-center lg:pl-96 md:pl-52 px-16">
+                    <div>
+                        <h1 class="lg:text-5xl text-3xl font-bold font-mono mb-3">Anas Mahmud</h1>
+                        <div className='flex align-middle'>
+                            <h1 className='lg:text-7xl text-4xl font-bold mr-6'>Web Developer</h1>
+                            <span className='divider bg-teal-700 h-[2px] w-16 lg:w-72 md:w-24 lg:mr-16 my-auto'></span>
+                        </div>
+                        <h1 className='lg:text-7xl text-5xl font-bold flex'>
+                            <span>+</span>
+                            <span>Front-End Developer.</span>
                         </h1>
-                        <span class="sm:block bg-gradient-to-r from-purple-600 via-green-300 to-blue-500 bg-clip-text text-3xl font-extrabold text-transparent sm:text-5xl">Front-End Developer.</span>
-
-                        {/* <p class="mx-auto mt-4 max-w-xl sm:text-xl sm:leading-relaxed">
-                            I’m a web developer specializing in react js. The main focus is front-end, but at the same time, I try to learn new technology also. Currently, I’m focused on learning mern stack development.
-                        </p> */}
                     </div>
-
                 </div>
-            </section>
 
-        </div>
+            </div>
+        </section>
     );
 };
 
