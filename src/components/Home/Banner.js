5558625e4703bffb66e68cb1141f593c53ecf6a9
Anas <anasmahmud523@gmail.com>
2023-01-17 14:04:28 +0600

    text color

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -7,7 +7,7 @@ const Banner = () => {
             <div class="w-full mx-auto pl:0  pt-16 h-[100vh] lg:h-screen">
 
                 <div class="lg:h-[60vh] h-[40vh] flex items-center lg:pl-80 md:pl-44 px-16">
-                    <div>
+                    <div className='text-gray-300'>
                         <h1 class="lg:text-5xl text-3xl font-bold font-mono mb-3">Anas Mahmud</h1>
                         <div className='flex align-middle'>
                             <h1 className='lg:text-7xl text-4xl font-bold mr-6'>Web Developer</h1>
